526fdfb9bf8be11ac10bcc3409ae621ffe9b365b
Nicolas Jimenez <nicolas.j2007@gmail.com>
2025-07-22 20:04:22 -0600

    Change background color

--- src/App.js
+++ src/App.js
@@ -8,18 +8,26 @@ import PostDetail from './pages/PostDetail'
 
 function App() {
   return (
-    <Router basename={process.env.NODE_ENV === 'production' ? '/blog-maggie' : '/'}>
-      <Navbar />
-      <main className='container mt-5 pt-5 flex-grow-1'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/blog' element={<Blog />} />
-          <Route path='/blog/:slug' element={<PostDetail />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <div
+      style={{
+        backgroundColor: '#E6E6FA', // Lila
+        minHeight: '100vh', // asegura cubrir toda la altura
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
+      <Router basename={process.env.NODE_ENV === 'production' ? '/blog-maggie' : '/'}>
+        <Navbar />
+        <main className='container mt-5 pt-5 flex-grow-1'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/blog' element={<Blog />} />
+            <Route path='/blog/:slug' element={<PostDetail />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </div>
   )
 }
 
